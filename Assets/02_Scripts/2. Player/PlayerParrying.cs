48daaa050aa7940b88a37a042a1900a569cd4cf2
kjp2981 <81199996+kjp2981@users.noreply.github.com>
2022-04-28 19:05:14 +0900

    parryinh code

--- Assets/02_Scripts/2. Player/PlayerParrying.cs
+++ Assets/02_Scripts/2. Player/PlayerParrying.cs
@@ -10,28 +10,66 @@ public class PlayerParrying : MonoBehaviour
     [SerializeField, Header("패링 성공시에 얻는 스테미나 양")]
     private int parringSuccessStemina = 2;
 
-    public KeyCode parryingKeyCode = KeyCode.F;
+    [SerializeField, Header("패링 키")]
+    private KeyCode parryingKeyCode = KeyCode.Return;
+    [SerializeField, Header("패링 딜레이")]
+    private float parryingDelay = .5f;
 
+    private float timer = 0f;
+
+    private bool isParrying = false;
+    private bool isAttack = false;
+
+    // 상대 공격 콜라이더에 닿았을 떄
+    // 상대가 공격 중일때
+    // 내가 패링 중인가?
     void Start()
     {
-        
+        timer = 0f;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(parryingKeyCode))
+        timer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (SteminaManager.Instance.CheckStemina(parriyngStemina))
+            StartCoroutine(EnemyAttack());
+        }
+
+        if (timer >= parryingDelay)
+        {
+            if (Input.GetKeyDown(parryingKeyCode))
             {
-                SteminaManager.Instance.MinusStemina(parriyngStemina);
-                Parrying();
+                if (SteminaManager.Instance.CheckStemina(parriyngStemina))
+                {
+                    StartCoroutine(ParryingCoroutine());
+                    SteminaManager.Instance.MinusStemina(parriyngStemina);
+                    Parrying();
+                    timer = 0f;
+                }
             }
         }
+    }
+
+    private IEnumerator EnemyAttack()
+    {
+        isAttack = true;
+        yield return new WaitForSeconds(.5f);
+        isAttack = false;
+    }
+
+    private IEnumerator ParryingCoroutine()
+    {
+        isParrying = true;
+        yield return new WaitForSeconds(.5f);
+        isParrying = false;
     }
 
     public void Parrying()
     {
         // TODO : Parring 행동하기
+        Debug.Log("패링");
         if (CheckParrying())
         {
             SuccessParyring();
@@ -49,15 +87,21 @@ public class PlayerParrying : MonoBehaviour
     private bool CheckParrying()
     {
         // 패링 성공시 true 반환, 실패시 false 반환
-        return false;
+        if (isAttack && isParrying)
+            return true;
+        else
+            return false;
     }
 
     /// <summary>
-    /// 패링 실패시에 행동 함수(구지 필요한가?)
+    /// 패링 실패시에 행동 함수
     /// </summary>
     private void FailedParrying()
     {
         // TODO : Failed Parring
+        Debug.Log("패링 실패!");
+        if(isAttack)
+            Debug.Log("데미지를 받았습니다.");
     }
 
     /// <summary>
@@ -66,6 +110,7 @@ public class PlayerParrying : MonoBehaviour
     private void SuccessParyring()
     {
         // TODO : Success Parring
+        Debug.Log("패링 성공!");
         SteminaManager.Instance.PlusStemina(parringSuccessStemina); // 스테미나 증가
     }
 }
