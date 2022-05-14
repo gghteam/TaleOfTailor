739e852ab7d7dab795f497169ded1d0159a16d39
kdh123456 <77655332+kdh123456@users.noreply.github.com>
2022-05-15 04:42:07 +0900

    허그 완성

--- Assets/02_Scripts/3. Enemy/Enemy_hug.cs
+++ Assets/02_Scripts/3. Enemy/Enemy_hug.cs
@@ -7,6 +7,11 @@ public class Enemy_hug : MonoBehaviour
 	[SerializeField]
 	private GameObject playerObject;
 
+	[SerializeField]
+	private Transform leftHand;
+	[SerializeField]
+	private Transform rightHand;
+
 	private Animator ani;
 	//잡는 시기 변수
 	private bool huging = false;
@@ -21,11 +26,10 @@ public class Enemy_hug : MonoBehaviour
 
 	private void Update()
 	{
-		Debug.Log(a());
-		if (a() < 1.5f && !ani.GetBool("hug"))
+		if (a() < 1.5f && !ani.GetBool("IsHug"))
 		{
 			//애니메이션을 플레이 시켜준다.
-			ani.SetBool("hug", true);
+			ani.SetBool("IsHug", true);
 			isfirst = true;
 		}
 		Hug();
@@ -34,15 +38,25 @@ public class Enemy_hug : MonoBehaviour
 
 	private void Hug()
 	{
-		if (ani.GetBool("hug") && (ani.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.4f && ani.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.3f))
+		if (ani.GetBool("IsHug") && (ani.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.4f && ani.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.3f))
 		{
 			huging = true;
 		}
-
-		if (!isfirst)
+		else if (ani.GetBool("IsHug") && ani.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.4f && isfirst)
 		{
-			ani.SetBool("hug", false);
+			ani.SetBool("IsHug", false);
 		}
+		else if (ani.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+		{
+			isfirst = true;
+			playerObject.transform.position = new Vector3(playerObject.transform.position.x, 1, playerObject.transform.position.z);
+			//데미지 달게하고
+		}
+		else if (!isfirst)
+		{
+			playerObject.transform.position = leftHand.transform.position;
+		}
+
 		//하다가 일정 시간 이상 안맞았을때 플레이를 그만 해주고
 		//아니면 계속 플레이해준다.
 		//맞았으면 멈춰주고 플레이어는 버둥된다.
@@ -53,10 +67,9 @@ public class Enemy_hug : MonoBehaviour
 
 	private void OnTriggerStay(Collider other)
 	{
-		if (huging && isfirst)
+		if (huging && isfirst && other.CompareTag("Player"))
 		{
 			isfirst = false;
-			//여기서 해주고
 		}
 	}
 
