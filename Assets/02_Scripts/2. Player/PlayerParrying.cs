27b24e08f507e4454b84e53c80b8da7f50018ed1
kjp2981 <81199996+kjp2981@users.noreply.github.com>
2022-05-14 21:53:47 +0900

    패링 완성

--- Assets/02_Scripts/2. Player/PlayerParrying.cs
+++ Assets/02_Scripts/2. Player/PlayerParrying.cs
@@ -18,20 +18,10 @@ public class PlayerParrying : Character
     [SerializeField, Tooltip("시야각 범위")]
     private float viewAngle = 60f;
 
-    private int parryingLayer = 1 << 7;
-
     private float timer = 0f;
 
     private bool isParrying = false;
     public bool IsParrying { get => isParrying; }
-
-    private Collider[] hitColl;
-
-    // 디버그용 코드 삭제 예정...? // 적이 여러명 있을 경우를 생각안함. 현재는 한명만 있을 경우로 가장하여 실행되는 거임.
-
-    // 상대 공격 콜라이더에 닿았을 떄
-    // 상대가 공격 중일때
-    // 내가 패링 중인가?
 
     private readonly int parrying = Animator.StringToHash("isParrying");
     void Start()
@@ -43,15 +33,7 @@ public class PlayerParrying : Character
     {
         timer += Time.deltaTime;
 
-        //Physics.Raycast(transform.position, transform.forward, out hit, 1, parryingLayer);
-
-        //hitColl = Physics.OverlapCapsule(transform.position, transform.position + new Vector3(0, 2.2f, 0), 1, parryingLayer);
         ani.SetBool(parrying, IsParrying);
-
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    StartCoroutine(EnemyAttack());
-        //}
 
         if (timer >= parryingDelay)
         {
@@ -67,6 +49,10 @@ public class PlayerParrying : Character
         }
     }
 
+    /// <summary>
+    /// 패링 코루틴
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator ParryingCoroutine()
     {
         isParrying = true;
@@ -74,83 +60,21 @@ public class PlayerParrying : Character
         isParrying = false;
     }
 
+    /// <summary>
+    /// 패링 함수
+    /// </summary>
     public void Parrying()
     {
         // TODO : Parring 행동하기
         Debug.Log("패링");
         StartCoroutine(ParryingCoroutine());
-
-        //if (hitColl == null)
-        //{
-        //    FailedParrying();
-        //}
-        //else
-        //{
-        //    ReturnParryingData();
-        //}
-        //if (CheckParrying())
-        //{
-        //    SuccessParyring();
-        //}
-        //else
-        //{
-        //    FailedParrying();
-        //}
     }
-
-    ///// <summary>
-    ///// 패링이 성공했는지 체크하는 함수
-    ///// </summary>
-    ///// <returns></returns>
-    //public bool CheckParrying()
-    //{
-    //    if (hit.collider == null) return false;
-    //    else
-    //    {
-    //        //Vector3 targetDir = (hit.collider.transform.position - transform.position);
-    //        //float dot = Vector3.Dot(transform.forward, targetDir);
-
-    //        //float theta = Mathf.Acos(dot) * Mathf.Rad2Deg;
-
-    //        //if (theta <= 60)// 내 시야각 안에 있을 때
-    //        //{
-    //        //    bool isAttack = hit.collider.GetComponent<EnemyAttack>().IsAttack;
-
-    //        //    // 패링 성공시 true 반환, 실패시 false 반환
-    //        //    if (isAttack && isParrying) // isAttack을 떄리는 적에게서 가져오기
-    //        //        return true;
-    //        //    else
-    //        //        return false;
-    //        //}
-    //        //else
-    //        //    return false;
-    //    }
-    //}
 
     /// <summary>
-    /// 가져온 콜라이어 각각에게 자신의 패링 정보를 넘겨주는 함수
+    /// tr과 나의 시야각을 계산해서 tr이 내 시야내에 있는지를 반환해주는 함수
     /// </summary>
-    private void ReturnParryingData()
-    {
-        foreach(var item in hitColl)
-        {
-            Vector3 targetDir = (item.transform.position - transform.position).normalized;
-            float dot = Vector3.Dot(transform.position, targetDir);
-
-            float theta = Mathf.Acos(dot) * Mathf.Rad2Deg;
-
-            if (theta <= viewAngle)
-            {
-                //item.GetComponent<EnemyAttack>().IsPlayerParrying = true;
-                item.GetComponent<EnemyAttack>().IsParrying(IsParrying);
-            }
-            else
-            {
-                //item.GetComponent<EnemyAttack>().IsPlayerParrying = false;
-            }
-        }
-    }
-
+    /// <param name="tr"></param>
+    /// <returns></returns>
     public bool IsInViewangle(Transform tr)
     {
         Vector3 targetDir = (tr.transform.position - transform.position).normalized;
--- Assets/02_Scripts/3. Enemy/EnemyAttack.cs
+++ Assets/02_Scripts/3. Enemy/EnemyAttack.cs
@@ -14,20 +14,17 @@ public class EnemyAttack : Character
     [SerializeField]
     private EnemyDataSO enemyData = null;
 
-    private int attackLlayer = 1 << 6;
+    private int attackLayer = 1 << 6;
 
     private bool isAttack = false;
-    public bool IsAttack { get => isAttack; }
-
-    private bool isParryingSuccess = false;
-
     private bool isPlayerDamage = false;
+    private bool isEnemyParrying = false;
 
     private int attackCount = 0;
 
     private float timer = 0f;
 
-    RaycastHit hit;
+    Collider[] hitColl;
 
     private readonly int attack = Animator.StringToHash("attack");
     private readonly int parrying = Animator.StringToHash("parrying");
@@ -47,51 +44,37 @@ public class EnemyAttack : Character
     {
         timer += Time.deltaTime;
 
-        Physics.Raycast(transform.position + new Vector3(0, 1.5f, 0), transform.forward, out hit, enemyData.attackRange, attackLlayer);
+        hitColl = Physics.OverlapCapsule(transform.position, new Vector3(0, 2.2f, 0), enemyData.attackRange, attackLayer);
+
         ani.SetFloat(attackCnt, attackCount % 2);
-        ani.SetBool(parrying, isParryingSuccess);
 
-        // 만약 내가 공격 중인데 플레이어가 패링 중이면 패링 성공
-        //  패링 중 이 아니명 플레이어 데미지 받기
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    if (timer >= enemyData.attackDelay)
+        //    {
+        //        if (!isAttack)
+        //        {
+        //            Attack();
+        //            timer = 0;
+        //        }
+        //    }
+        //}
 
-        Debug.DrawRay(transform.position + new Vector3(0, 1.5f, 0), transform.forward, Color.green);
-
-        if (Input.GetKeyDown(KeyCode.A))
+        if (timer >= enemyData.attackDelay)
         {
-            if (timer >= enemyData.attackDelay)
+            if (!isAttack)
             {
-                if (!isAttack)
-                {
-                    Attack();
-                    timer = 0;
-                }
+                Attack();
+                timer = 0;
             }
         }
-
-        
-    }
-
-    private void FailedParrying()
-    {
-        hit.collider.GetComponent<PlayerParrying>()?.FailedParrying();
-        Debug.Log("패링 실패");
-        Debug.Log("Player Damage");
-        isPlayerDamage = true;
-    }
-
-    private void SuccessParrying()
-    {
-        Debug.Log("패링 성공");
-        hit.collider.GetComponent<PlayerParrying>()?.SuccessParrying();
-        ParryingAction();
     }
 
     /// <summary>
-    /// 공격당했을 때 함수
+    /// 패링당했을 때 함수
     /// </summary>
     private void ParryingAction()
     {
-        // 패링 당했을 때 행동
         Debug.Log("쫌 치네ㅋ");
     }
 
@@ -100,50 +83,29 @@ public class EnemyAttack : Character
     /// </summary>
     private void Attack()
     {
-        /*
-        방법 2. 공격 중에는 적에 앞 방향으로 레이케스트를 쏜다 -> 애를 쓰자
-         그 레이에 닿으면 Damage주기
-         */
-
-        // 에니메이션 마지막에 Event달기
-        //  AttackChanged, ColliderEnabledChanged를 Event로 등록함
         attackCount++;
-        AttackChanged(1);
+        AttackChange(1);
         ani.SetTrigger(attack);
+
+        //if (isAttack)
+        //{
+        //    if (hit.collider != null)
+        //    {
+        //        if (hit.collider.CompareTag("Player"))
+        //        {
+        //            //StartCoroutine(AttackCoroutine());
+        //        }
+        //    }
+        //}
 
         if (isAttack)
         {
-            if (hit.collider != null)
+            foreach(var hitObj in hitColl)
             {
-                if (hit.collider.CompareTag("Player"))
+                if (hitObj.CompareTag("Player"))
                 {
-                    // 애니메이션이 일정 시간 재생되었을 때
-                    //  isParryingSuccess가 true면 패링 당해버렸!
-                    //      아니면 데미지 져버렸!
-
-                    //if(ani.GetCurrentAnimatorStateInfo(0).IsName("Attack Blend Tree") &&
-                    //    ani.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f &&
-                    //    ani.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.5f)
-                    //if (!isPlayerDamage)
-                    //{
-                    //    if (isParryingSuccess)
-                    //    {
-                    //        hit.collider.GetComponent<PlayerParrying>().SuccessParrying();
-                    //        ParryingAction();
-                    //    }
-                    //    else
-                    //    {
-                    //        hit.collider.GetComponent<PlayerParrying>().FailedParrying();
-                    //        Debug.Log("플레이어 대미지 주기");
-                    //        isPlayerDamage = true;
-                    //    }
-                    //}
-                    StartCoroutine(AttackCoroutine());
+                    StartCoroutine(AttackCoroutine(hitObj.gameObject));
                 }
-            }
-            else
-            {
-                isParryingSuccess = false;
             }
         }
     }
@@ -152,12 +114,13 @@ public class EnemyAttack : Character
     /// 공격 코루틴
     /// </summary>
     /// <returns></returns>
-    private IEnumerator AttackCoroutine()
+    private IEnumerator AttackCoroutine(GameObject hitObj)
     {
         yield return new WaitForSeconds(.5f);
+
         if (!isPlayerDamage)
         {
-            PlayerParrying player = hit.collider.GetComponent<PlayerParrying>();
+            PlayerParrying player = hitObj.GetComponent<PlayerParrying>();
             bool isParrying = player.IsParrying;
 
             if (isParrying)
@@ -165,11 +128,13 @@ public class EnemyAttack : Character
                 if (player.IsInViewangle(this.transform))
                 {
                     player.SuccessParrying();
+                    ani.SetBool(parrying, true);
                     ParryingAction();
                 }
                 else
                 {
                     player.FailedParrying();
+                    ani.SetBool(parrying, false);
                     Debug.Log("플레이어 대미지 주기");
                     isPlayerDamage = true;
                 }
@@ -177,32 +142,23 @@ public class EnemyAttack : Character
             else
             {
                 player.FailedParrying();
+                ani.SetBool(parrying, false);
                 Debug.Log("플레이어 대미지 주기");
                 isPlayerDamage = true;
             }
         }
     }
 
-    [System.Obsolete]
-    public void IsParrying(bool isParrying)
+    private void ParryingChange()
     {
-        if(isParrying && isAttack)
-        {
-            Debug.Log("패링 성공");
-            isParryingSuccess = true;
-        }
-        else
-        {
-            Debug.Log("패링 실패");
-            isParryingSuccess = false;
-        }
+        ani.SetBool(parrying, false);
     }
 
     /// <summary>
     /// isAttack변수(현재 공격중인지?)를 변경시키는 함수
     /// </summary>
     /// <param name="value"></param>
-    private void AttackChanged(int value)
+    private void AttackChange(int value)
     {
         isAttack = value == 0 ? false : true;
     }
@@ -211,7 +167,7 @@ public class EnemyAttack : Character
     /// isPlayerDamage변수(플레이어가 데미지를 받는 중인지?)를 변경시키는 함수. 도트뎀을 막는 용도
     /// </summary>
     /// <param name="value"></param>
-    private void PlayerDamageChanged(int value)
+    private void PlayerDamageChange(int value)
     {
         isPlayerDamage = value == 0 ? false : true;
     }
